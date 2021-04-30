c1c15c1b91ebffc62c3f37093e0f70130baabf35
juniq <juniqlim@gmail.com>
2021-05-01 00:18:49 +0900

    feat: 자동차 전진
    랜덤 숫자(0~9) 생성 후 4이상이면 1보 전진

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,18 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
     }
 
     public void move() {
+        if (new Random().nextInt(9) >= 4) {
+            position++;
+        }
     }
 }
