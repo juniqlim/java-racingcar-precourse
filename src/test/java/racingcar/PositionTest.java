9656f09565f738e38c801dd8f91a830b275f4c9b
juniq <juniqlim@gmail.com>
2021-05-02 00:48:09 +0900

    test: 메소드명 수정

--- src/test/java/racingcar/PositionTest.java
+++ src/test/java/racingcar/PositionTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class PositionTest {
     @Test
-    void increase() {
+    void move() {
         Position position = Position.of(0);
 
         position.move(() -> true);
