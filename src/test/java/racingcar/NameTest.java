295fbfb1d6d15f8e95093665ebb685fa0186237a
juniq <juniqlim@gmail.com>
2021-05-01 22:44:11 +0900

    test: 자동차 이름은 5자 이하

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +9,11 @@ class NameTest {
     @Test
     void create() {
         assertThatCode(() -> new Name("juniq")).doesNotThrowAnyException();
+    }
+
+    @Test
+    void validNameLength() {
+        assertThatThrownBy(() -> new Name("juniqlim")).isInstanceOf(NameLengthException.class)
+            .hasMessageMatching("자동차 이름은 5자 이하여야 합니다.");
     }
 }
