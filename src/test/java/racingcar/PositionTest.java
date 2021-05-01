3b1c004e817ab283e31afe0f2e7da2971507fee3
juniq <juniqlim@gmail.com>
2021-05-01 23:19:54 +0900

    test: 자동차 위치 증가

--- src/test/java/racingcar/PositionTest.java
+++ src/test/java/racingcar/PositionTest.java
@@ -5,8 +5,15 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import org.junit.jupiter.api.Test;
 
 class PositionTest {
+    private static final Position position = Position.of(0);
+
     @Test
     void create() {
-        assertThatCode(() -> new Position(0)).doesNotThrowAnyException();
+        assertThatCode(() -> Position.of(0)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void increase() {
+        assertThat(position.increase()).isEqualTo(Position.of(1));
     }
 }
