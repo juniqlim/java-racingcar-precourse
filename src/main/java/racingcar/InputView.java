d6f3077140a9f92ef1a5b800aecfdca646cd1a5b
juniq <juniqlim@gmail.com>
2021-05-02 16:28:03 +0900

    feat: 입력
    자동차 이름 입력
    시도할 회수 입력

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String names() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.next();
+    }
+
+    public static int tryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+}
