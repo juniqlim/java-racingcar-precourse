ef1035628a9267f0f4d46f34d831a71ecb807f6a
juniq <juniqlim@gmail.com>
2021-05-03 22:25:09 +0900

    test: 중복 테스트 삭제

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,17 +1,11 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
-    @Test
-    void create() {
-        assertThatCode(() -> Car.of("juniq")).doesNotThrowAnyException();
-    }
-
     @Test
     void move() {
         Car car = Car.of("juniq");
