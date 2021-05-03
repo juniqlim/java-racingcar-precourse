a861b167b1872f6e7142e8ba26af21eee3c4f738
juniq <juniqlim@gmail.com>
2021-05-03 22:11:07 +0900

    test: 출력 - 최종 우승자 출력

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class ResultView {
     public static void printScore(Cars cars) {
         for (Car car : cars) {
@@ -14,5 +16,19 @@ public class ResultView {
             positionGraph += "-";
         }
         return positionGraph;
+    }
+
+    public static void printWinner(List<Car> winners) {
+        for (int i = 0; i < winners.size(); i++) {
+            addSeparatorIfNotFirst(i);
+            System.out.print(winners.get(i).nameValue());
+        }
+        System.out.println("가 최종 우승했습니다.");
+    }
+
+    private static void addSeparatorIfNotFirst(int index) {
+        if (index != 0) {
+            System.out.print(", ");
+        }
     }
 }
