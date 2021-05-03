f84e08024be43f860c738cc4c428b943f2e2e33e
juniq <juniqlim@gmail.com>
2021-05-03 22:24:35 +0900

    test: Car 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
@@ -18,5 +19,17 @@ class CarTest {
         car.move(() -> true);
 
         assertThat(car).usingRecursiveComparison().isEqualTo(Car.of("juniq", 1));
+    }
+
+    @Test
+    @DisplayName("더 큰 위치를 반환")
+    void largerPosition() {
+        assertThat(Car.of("juniq").largerPosition(1)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("같은 포지션인지 확인")
+    void equalPosition() {
+        assertThat(Car.of("juniq").equalPosition(1)).isFalse();
     }
 }
