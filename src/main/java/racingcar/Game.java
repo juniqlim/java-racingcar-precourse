a011a6ba7e2f3b985844f82b5a63d5418b0b8b5d
juniq <juniqlim@gmail.com>
2021-05-03 22:15:20 +0900

    repactor: 메소드 추출

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -3,11 +3,14 @@ package racingcar;
 public class Game {
     public static void main(String[] args) {
         Cars cars = Cars.of(InputView.names());
-        int tryCount = InputView.tryCount();
+        racing(cars, InputView.tryCount());
+        ResultView.printWinner(cars.winners());
+    }
+
+    private static void racing(Cars cars, int tryCount) {
         for (int i = 0; i < tryCount; i++) {
             cars.move(new RandomMoveStrategy());
             ResultView.printScore(cars);
         }
-        ResultView.printWinner(cars.winners());
     }
 }
