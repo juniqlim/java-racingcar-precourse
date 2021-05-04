b741c40f418984d0943af3cbc58cd94fa6050e39
juniq <juniqlim@gmail.com>
2021-05-04 22:57:41 +0900

    repactor: 상수명 수정
    좀 더 어울리는 이름으로 수정

--- src/main/java/racingcar/domain/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class RandomMoveStrategy implements MoveStrategy {
 
-    public static final int BOUND = 9;
-    public static final int MAGIC_NUMBER = 4;
+    public static final int BOUND_OF_RANDOM_NUMBER = 9;
+    public static final int CRITERIA_OF_CAR_MOVEMENT = 4;
 
     @Override
     public boolean movable() {
-        return new Random().nextInt(BOUND) >= MAGIC_NUMBER;
+        return new Random().nextInt(BOUND_OF_RANDOM_NUMBER) >= CRITERIA_OF_CAR_MOVEMENT;
     }
 }
